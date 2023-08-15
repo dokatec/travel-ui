e6c88097d618ba0f0658d8e2d67f193973d08dcc
Jhonatan Pacheco da Silva <47837212+dokatec@users.noreply.github.com>
2023-08-15 13:15:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+//header
+
+AOS.init();
+
+// slider images array
+
+const sliderImgs = [
+  "img1.png",
+  "img2.png",
+  "img3.png",
+  "img4.png",
+  "img5.png",
+  "img6.png",
+];
+let sliderImage = document.querySelector(".background-image");
+let sliderGrids = [...document.querySelectorAll(".grid-item")];
+
+let currentImage = 0;
+
+setInterval(() => {
+  changeSliderImage();
+}, 5000);
+
+const changeSliderImage = () => {
+  sliderGrids.map((gridItem, index) => {
+    setTimeout(() => {
+      gridItem.classList.remove("hide");
+
+      setTimeout(() => {
+        if (index == sliderGrids.length - 1) {
+          if (currentImage >= sliderImgs.length - 1) {
+            currentImage = 0;
+          } else {
+            currentImage++;
+          }
+
+          sliderImage.src = `img/${sliderImgs[currentImage]}`;
+
+          sliderGrids.map((item, i) => {
+            setTimeout(() => {
+              item.classList.add("hide");
+            }, i * 100);
+          });
+        }
+      }, 100);
+    }, index * 100);
+  });
+};
